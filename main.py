3cf7503f0bb356acddc3f63f06b932e344a08855
Vansh Khurana <vansh21k@gmail.com>
2014-02-15 21:47:29 +0530

    Added Iterative solution

--- main.py
+++ main.py
@@ -8,15 +8,27 @@ def getCycles(n):
     else:
         return 1 + getCycles(n/2)
         
+def getCyclesIterative(n):
+    cycles = 1
+    while(n != 1):
+        cycles = cycles + 1
+        if (n%2 == 0):
+            n = n/2
+        else:
+            n = 3 * n + 1
+    return cycles
+        
+        
 inp = raw_input("Enter 2 numbers seperated by space\n")
 a,b = inp.strip().split(' ')
 a = int(a)
 b = int(b)
+#print a,b
 if (a > b):
     a,b = b,a #Swaps integers, a should be lesser than b
 max_cycle, cur_cycle = 0, 0 #max_cycle to keep state of current max, cur_cycle to keep state of current
 for i in range(a,b+1):
-    cur_cycle = getCycles(i)
+    cur_cycle = getCyclesIterative(i)
     if cur_cycle > max_cycle:
         max_cycle = cur_cycle
 print max_cycle
